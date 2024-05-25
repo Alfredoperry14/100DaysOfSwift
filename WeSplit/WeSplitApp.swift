fbce8ec003398c0f25d3a13c0780626a6d59f326
Alfredo Perry <alfredoperry14@gmail.com>
2024-05-25 13:24:33 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alfredo Perry on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
